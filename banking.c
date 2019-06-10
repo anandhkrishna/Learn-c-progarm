5e0ffceeb8d3242b7d07655ccc5c02b7ea49c10e
anandhkrishna <45140249+anandhkrishna@users.noreply.github.com>
2019-06-10 19:53:25 +0530

    Add files via upload

---
+++ banking.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+int main()
+{
+	int a[100],f,c,;
+	printf("1.do you have headache?\n");
+	scanf("%c",&a);
+	if(strcmp(a,'y')==0)
+	{
+		printf("");
+		f++;
+	}
+	printf("2.do  you have wamitting scense?\n");
+	scanf("%c",&a);
+	if(strcmp(a,y)==0)
+	{
+		f++;
+	}
+	printf("3.....");
+	scanf("%c",&a);
+	if(strcmp(a,'y')==0)
+	{
+		f++;
+	printf("4.....");
+	scanf("%c",&a);
+	if(strcmp(a,'y')==0)
+	{
+		f++;
+	}
+	printf("5.....");
+	scanf("%c",&a);
+	if(strcmp(a,'y')==0)
+	{
+		c++;
+	}
+	printf("6.....");
+	scanf("%c",&a);
+	if(strcmp(a,'y')==0)
+	{
+		c++;
+	}
+	printf("7.....");
+	scanf("%c",&a);
+	if(strcmp(a,'y')==0)
+	{
+		c++;
+	}
+	printf("8.....");
+	scanf("%c",&a);
+	if(strcmp(a,'y')==0)
+	{
+		c++;
+	}
+	printf("9.....");
+	scanf("%c",&a);
+	if(strcmp(a,))
+	printf("10.....");
+	scanf("%c",&a);
+	
+
